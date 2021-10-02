0089cb624df6b08a44f0905b99e674f06cd09489
Vasily Himchenko <White_Scorpion@inbox.lv>
2021-10-02 21:13:27 +0300

    Edded tinncures: Ermine, Vair, Sanguine, Murrey, Tenne Ermine & Vair - added inot enum, but noiot implemented.

--- SignumGenerator/SignumGenerator/Signum/SignumColor.cs
+++ SignumGenerator/SignumGenerator/Signum/SignumColor.cs
@@ -11,7 +11,12 @@ namespace SignumGenerator.Signum
         Purpure, // Purple
         Sable, // Black
         Or, // 255, 215, 0
-        Argent // 192, 192, 192 left as White? 
+        Argent, // 192, 192, 192 left as White? 
+        Ermine,
+        Vair,
+        Sanguine,
+        Murrey,
+        Tenne
     }
 
     public static class SignumColor
@@ -27,6 +32,9 @@ namespace SignumGenerator.Signum
                 ETincture.Vert => Color.FromArgb(255, 0, 75, 0),
                 ETincture.Or => Color.FromArgb(255, 255, 215, 0),
                 ETincture.Argent => Color.FromArgb(255, 192, 192, 192),
+                ETincture.Sanguine => Color.FromArgb(255, 188, 63, 74),
+                ETincture.Murrey => Color.FromArgb(255, 139, 0, 75),
+                ETincture.Tenne => Color.FromArgb(255, 205, 87, 00),
                 _ => Color.FromArgb(255, 0, 0, 0)
             };
         }
